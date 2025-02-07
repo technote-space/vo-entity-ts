39b878547ccc571215768e8771f999e48e6ed6f9
Technote <technote.space@gmail.com>
2025-02-07 23:16:06 +0900

    chore: tweaks

--- src/exceptions/exception.ts
+++ src/exceptions/exception.ts
@@ -1,10 +1,10 @@
 export type ErrorStatus = 400 | 401 | 403 | 404 | 409 | 422 | 500;
 
 export default class Exception extends Error {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   public constructor(
     public readonly status: ErrorStatus,
     message: string,
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     public readonly context?: Record<string, any>,
   ) {
     super(message);
