65eadb17354c6a8ee2f6b706a50eafb463fcb75e
Technote <technote.space@gmail.com>
2025-05-20 01:58:33 +0900

    fix: error code

--- src/exceptions/exception.ts
+++ src/exceptions/exception.ts
@@ -1,4 +1,16 @@
-export type ErrorStatus = 400 | 401 | 403 | 404 | 409 | 422 | 500;
+type ToNumber<T extends string> = T extends `${infer N extends number}`
+  ? N
+  : never;
+type Digit = '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9';
+type ClientErrorStatusCode =
+  | `40${Digit}`
+  | `41${Digit}`
+  | `42${Digit}`
+  | '431'
+  | '451';
+type ServerErrorStatusCode = `50${Digit}` | '510' | '511';
+type HttpErrorStatusCode = ClientErrorStatusCode | ServerErrorStatusCode;
+export type ErrorStatus = ToNumber<HttpErrorStatusCode>;
 
 export class Exception extends Error {
   public constructor(
